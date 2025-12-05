fb36bad5f527b162a75b5c1610b70025b70f5ff1
vtion001 <generalmanager.tiongsons@gmail.com>
2025-12-05 21:49:44 +0800

    feat(hero): add logo link to home in hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -32,6 +32,16 @@ const itemVariants = {
 export default function Hero() {
   return (
     <div className="relative w-full h-screen overflow-hidden bg-[#0a0a0a]">
+      <a href="/" className="absolute top-6 left-6 z-30" aria-label="Go to Home">
+        <Image
+          src="https://res.cloudinary.com/dbviya1rj/image/upload/v1764835951/yte8v4vubwe6cdvfncas.png"
+          alt="ISTRAT logo"
+          width={600}
+          height={240}
+          className="h-40 w-auto opacity-90 drop-shadow"
+          priority
+        />
+      </a>
       
       {/* Three.js Liquid Chrome Background */}
       <div className="absolute inset-0 z-0">
