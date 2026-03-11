9d5a2ee0b859973c4627f6592587a384d3452f33
vtion001 <generalmanager.tiongsons@gmail.com>
2026-03-11 17:16:25 +0800

    feat: reduce SVG size by 10% in core values section

--- components/home/HomeCoreValues.tsx
+++ components/home/HomeCoreValues.tsx
@@ -46,7 +46,7 @@ export default function HomeCoreValues() {
 
                                             {/* SVG Container: Positioned higher on mobile to avoid text overlap */}
                                             {value.svgUrl ? (
-                                                <div className="absolute top-[35%] md:top-[42%] lg:top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-[140%] h-[25vh] sm:w-[150%] sm:h-[50vh] md:w-[200%] md:h-[1400px] lg:h-[1600px] pointer-events-none z-0">
+                                                <div className="absolute top-[35%] md:top-[42%] lg:top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-[126%] h-[22.5vh] sm:w-[135%] sm:h-[45vh] md:w-[180%] md:h-[1260px] lg:h-[1440px] pointer-events-none z-0">
                                                     <Image
                                                         src={value.svgUrl}
                                                         alt={value.title}
